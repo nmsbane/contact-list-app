e538c3958d2847cc34d5cd125e797c84764267c3
nmsbane <madhusudhan638@gmail.com>
2016-03-27 23:11:51 +0530

    used dummy persons data to fill the table, that dummy data is coming from the server and angular is fetching the data using .get method

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,13 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('AppCtrl', ["$scope", function($scope) {
+myApp.controller('AppCtrl', ["$scope", "$http",  function($scope, $http) {
 	console.log("hello world from controller");
+
+	$http.get('/contactlist').success(function(response) {
+		console.log(response);
+		$scope.contactList = response;
+	})
+	
+	
+	
 }]);
--- server.js
+++ server.js
@@ -3,4 +3,28 @@ var app = express();
 
 app.use(express.static(__dirname+'/public'));
 
+app.get('/contactlist', function(req, res) {
+	console.log("i received a get request");
+	var person1  ={
+		name: 'tim',
+		email: 'tim@cook.com',
+		number: '(111)-111-111'
+	};
+
+	var person2 = {
+		name: 'emily',
+		email: 'emily@email2.com',
+		number: '(222)-222-2222'
+	};
+
+	var person3 = {
+		name: 'john',
+		email: 'john@email3.com',
+		number: '(333)-333-3333'
+	};
+
+	var contactList = [person1, person2, person3];
+	res.json(contactList);
+});
+
 app.listen(3000);
