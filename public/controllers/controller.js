c4fc4197d0adb2b6da51a4289e446b786d9ea702
nmsbane <madhusudhan638@gmail.com>
2016-03-31 00:26:08 +0530

    completed with create and retrival functionality for adding contacts and getting contacts, used the existing collection from mongolab db, learnt how to query and create entries using mongoose and also got some practise with .get and .post methods

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,11 +3,24 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ["$scope", "$http",  function($scope, $http) {
 	console.log("hello world from controller");
 
-	$http.get('/contactlist').success(function(response) {
-		console.log(response);
-		$scope.contactList = response;
-	})
+	var refresh = function() {
+		$http.get('/contactlist').success(function(response) {
+			console.log(response);
+			$scope.contactList = response;
+		});
+
+		$scope.contact = '';
+	}
+	refresh();
+
 	
+	$scope.addContact = function(){
+		$http.post('/contactlist', $scope.contact).success(function(response) {
+			console.log("calling refresh function");
+			refresh();
+		});
+
+	}	
 	
 	
 }]);
--- server.js
+++ server.js
@@ -1,30 +1,47 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// connect to the mongodb 
+mongoose.connect('mongodb://root:root@ds037155.mlab.com:37155/banescontactlist', function(error, success) {
+	if(error){
+		console.log(error);
+	}
+});
+
+var Schema = mongoose.Schema;
+var contactSchema = new Schema({
+	name: String,
+	email: String,
+	number: String
+}, {collection: 'contactlist'});
+
+var contactlist = mongoose.model('contactlist', contactSchema);
+
 
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res) {
-	console.log("i received a get request");
-	var person1  ={
-		name: 'tim',
-		email: 'tim@cook.com',
-		number: '(111)-111-111'
-	};
+	contactlist.find({}, function(err, contactList) {
+		if (err) throw err;
 
-	var person2 = {
-		name: 'emily',
-		email: 'emily@email2.com',
-		number: '(222)-222-2222'
-	};
-
-	var person3 = {
-		name: 'john',
-		email: 'john@email3.com',
-		number: '(333)-333-3333'
-	};
-
-	var contactList = [person1, person2, person3];
-	res.json(contactList);
+		res.json(contactList);
+	});
 });
+
+app.post('/contactlist', function(req, res) {
+	var data = req.body;
+	var newContact = new contactlist({
+		name: data.name,
+		email: data.email,
+		number: data.number
+	});
+	newContact.save(function(err, docs){
+		res.json(docs);
+	})
+
+})
 
 app.listen(3000);
